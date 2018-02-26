6a07d79d51c36f94ae68d4e2e1c7a3e0a69458b2
Alex Levin <alexlevinb@gmail.com>
2018-02-25 19:09:32 -0500

    updated EventsController

--- src/main/java/io/spring/demo/issuesdashboard/events/EventsController.java
+++ src/main/java/io/spring/demo/issuesdashboard/events/EventsController.java
@@ -20,9 +20,19 @@ public class EventsController {
 		this.repository = repository;
 	}
 
+	/**
+	 * Fetches latest events for the repo
+	 * Endpoint URL: http://localhost:8080/events/spring-boot
+	 * @param repoName
+	 * @return array of the events
+	 */
 	@GetMapping("/events/{repoName}")
 	@ResponseBody
 	public RepositoryEvent[] fetchEvents(@PathVariable String repoName) {
+		// h2 db has a row with ID:1, ORG_NAME:spring_projects, REPO_NAME:spring-boot
+		// DB is populated by Flyway from V1.0.1_Insert_data.sql in the 
+		// resources/db/migration folder.
+		// The endpoint URL: https://api.github.com/repos/spring-projects/spring-boot/issues/events
 		GithubProject project = this.repository.findByRepoName(repoName);
 		return this.githubClient
 				.fetchEvents(project.getOrgName(), project.getRepoName()).getBody();
